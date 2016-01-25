065e2209ad8fc0ef7017e34ae4ceecf618edc069
Joa Ebert <joa.ebert@defrac.com>
2016-01-25 17:53:04 +0100

    Missing files (thanks IntelliJ)

---
+++ src/java/defrac/gaming/SampleScreen.java
@@ -0,0 +1,46 @@
+package defrac.gaming;
+
+import defrac.ui.*;
+
+/**
+ *
+ */
+public final class SampleScreen extends Screen {
+  private DisplayList displayList;
+
+  @Override
+  protected void onCreate() {
+    super.onCreate();
+
+    final LinearLayout layout =
+        LinearLayout.horizontal().gravity(Gravity.CENTER);
+
+    final LinearLayout.LayoutConstraints layoutConstraints =
+        new LinearLayout.LayoutConstraints();
+
+    layoutConstraints.width(800, PixelUnits.LP);
+    layoutConstraints.height(600, PixelUnits.LP);
+    layoutConstraints.gravity = Gravity.CENTER;
+
+    displayList = new DisplayList();
+    displayList.layoutConstraints(layoutConstraints);
+
+    layout.addView(displayList);
+    rootView(layout);
+
+    displayList.onStageReady(LayoutSample::new);
+
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    displayList.onPause();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    displayList.onResume();
+  }
+}
