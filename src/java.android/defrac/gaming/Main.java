f55f8ecacabc3950827488d9abd8c7d77116766b
Joa Ebert <joaebert@googlemail.com>
2016-03-21 14:27:15 +0100

    Update for 0.3.115
    We removed the @UsesSdk annotation in favor of the following configuration options:
    - `android:config minSdkVersion "123"`
    - `android:config targetSdkVersion "lollipop"`
    - `android:config maxSdkVersion "n"`
    
    The compiler has to know the minimum supported SDK version before compiling actual .class files. Therefore the annotation cannot be used.

--- src/java.android/defrac/gaming/Main.java
+++ src/java.android/defrac/gaming/Main.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Build;
 import defrac.dni.Activity;
 import defrac.dni.IntentFilter;
-import defrac.dni.UsesSdk;
 import defrac.ui.Screen;
 import defrac.ui.ScreenActivity;
 import defrac.ui.ScreenStack;
@@ -12,7 +11,6 @@ import defrac.ui.ScreenStack;
 import javax.annotation.Nonnull;
 
 @Activity(label = "defrac-ui", filter = @IntentFilter(action = Intent.ACTION_MAIN, category = Intent.CATEGORY_LAUNCHER))
-@UsesSdk(minSdkVersion = Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 public final class Main extends ScreenActivity {
   @Override
   @Nonnull
